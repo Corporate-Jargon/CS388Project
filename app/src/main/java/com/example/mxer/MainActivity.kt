d0ceeb4d764aa7daf895b5b87486bb255ef9816e
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2022-11-16 19:22:57 -0500

    Start of login and register changes

--- app/src/main/java/com/example/mxer/MainActivity.kt
+++ app/src/main/java/com/example/mxer/MainActivity.kt
@@ -11,31 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val fragmentManager: FragmentManager = supportFragmentManager
-//        findViewById<BottomNavigationView>(com.parse.R.id.bottom_navigation).setOnItemSelectedListener {
-//            // Alias
-//                item ->
-//            var fragmentToShow: Fragment? = null
-//            when (item.itemId) {
-////                com.parse.R.id.action_home -> {
-////                    fragmentToShow = BrowseFragment()
-////                }
-////                com.parse.R.id.action_event -> {
-////                    fragmentToShow = ComposeFragment()
-////
-////                }
-////                com.parse.R.id.action_profile -> {
-////                    fragmentToShow = ProfileFragment()
-////                }
-//            }
-//            if (fragmentToShow != null) {
-//                fragmentManager.beginTransaction().replace(com.parse.R.id.flContainer, fragmentToShow).commit()
-//            }
-//            // Return true when we handled the interaction
-//            true
-//        }
-//        // Set default selection
-//        findViewById<BottomNavigationView>(com.parse.R.id.bottom_navigation).selectedItemId = com.parse.R.id.action_home
     }
 }
