1df9ea2d57f9b743c5d57aa5f7f4569c76e87a01
Strevia <phillipnissen7@gmail.com>
2022-11-17 16:41:33 -0500

    add Comment dataclass

---
+++ app/src/main/java/com/example/mxer/Comment.kt
@@ -0,0 +1,34 @@
+package com.example.mxer
+
+import com.parse.ParseClassName
+import com.parse.ParseObject
+import com.parse.ParseUser
+
+@ParseClassName("Comment")
+class Comment: ParseObject() {
+    fun getAuthor(): ParseUser? {
+        return getParseUser(KEY_AUTHOR)
+    }
+    fun setAuthor(author: ParseUser){
+        put(KEY_AUTHOR, author)
+    }
+    fun getPost(): ParseObject? {
+        return getParseObject(KEY_REPLY)
+    }
+    fun setPost(post: ParseObject){
+        put(KEY_REPLY, post)
+    }
+    fun getDescription(): String? {
+        return getString(KEY_DESCRIPTION)
+    }
+    fun setDescription(desc: String) {
+        put(KEY_DESCRIPTION, desc)
+    }
+
+
+    companion object {
+        const val KEY_AUTHOR = "author"
+        const val KEY_REPLY = "reply_to"
+        const val KEY_DESCRIPTION = "description"
+    }
+}
