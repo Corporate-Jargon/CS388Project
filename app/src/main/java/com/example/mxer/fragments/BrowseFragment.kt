ce58882681ebca393c15b195a202c17993150908
Jeffrey Zhang <jz575@njit.edu>
2022-11-22 22:43:15 -0500

    added floating compose action, added to browse temporarily

--- app/src/main/java/com/example/mxer/fragments/BrowseFragment.kt
+++ app/src/main/java/com/example/mxer/fragments/BrowseFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.example.mxer.ComposeActivity
 import com.example.mxer.R
 
-class BrowseFragment : Fragment() {
+open class BrowseFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,5 +20,9 @@ class BrowseFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //Move below code to Community View once it is built, move the floating button in the xml
+        view.findViewById<Button>(R.id.btnCompose).setOnClickListener{
+            //TODO have the activity change the view
+        }
     }
 }
