55038f4e2239f9313d6d0148b99f2b22b365a9cc
Strevia <phillipnissen7@gmail.com>
2022-11-25 12:35:17 -0500

    added loading of profile picture, username, and description for current user

--- app/src/main/java/com/example/mxer/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/mxer/fragments/ProfileFragment.kt
@@ -1,13 +1,17 @@
 package com.example.mxer.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.mxer.R
+import com.parse.ParseUser
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
@@ -20,6 +24,16 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val tvUserName = view.findViewById<TextView>(R.id.username)
+        val tvPrimeCommunity = view.findViewById<TextView>(R.id.primaryCommunity)
+        val tvBio = view.findViewById<TextView>(R.id.bio)
+        val ivPfp = view.findViewById<ImageView>(R.id.profilePicture)
+
+        val user = ParseUser.getCurrentUser()
+        tvUserName.text = user.username
+        tvBio.text = user.getString("description")
+        Glide.with(requireContext()).load(user.getParseFile("profile_picture")?.url).into(ivPfp)
 
 
     }
